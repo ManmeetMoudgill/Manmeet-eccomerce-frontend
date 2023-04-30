837b6be00a5154cd5f72979fb38dbf1a1eb0fcd8
Navi <navbhartimoudgill123@gmail.com>
2023-04-30 18:31:19 +0200

    files added

--- components/home/best-offers-carousel.tsx
+++ components/home/best-offers-carousel.tsx
@@ -1,7 +1,8 @@
 //@ts-nocheck
 import Image from "next/image";
 import Carousel from "react-elastic-carousel";
-
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { AiOutlineHeart } from "react-icons/ai";
 interface OffersItems {
   id: number;
   src: string;
@@ -118,7 +119,7 @@ const BestOffersCarousel = () => {
         >
           {items.map((item) => (
             <div
-              className=" rounded-md shadow-md my-4 border-2 relative  p-4"
+              className="rounded-md cursor-pointer group hover:shadow-lg  my-4 border-2 relative  p-4"
               key={item?.id}
             >
               {/* SCONTO TAG HERE */}
@@ -146,20 +147,37 @@ const BestOffersCarousel = () => {
                 </div>
               </div>
 
-              <div className="flex flex-col mt-16 items-center justify-center">
-                <div className="text-blue-400 flex items-center font-medium text-lg">
-                  <span>Risparmio il </span>
-                  <span className=" font-medium text-blue-400 ml-2">
-                    -{item?.discount}%
-                  </span>
-                </div>
-                <div className="flex items-center justify-center ">
-                  <span className="text-lg px-2 line-through text-gray-600">
-                    € {item?.price}
-                  </span>
-                  <span className="text-lg font-bold px-2">
-                    € {item?.discountPrice}
-                  </span>
+              <div className="flex   flex-col mt-16 items-center justify-center">
+                <div className="flex relative  justify-center items-center">
+                  <div className="flex relative  opacity-1   group-hover:opacity-0 transition-all flex-col mt-16 items-center justify-center">
+                    <div className="text-blue-400 flex items-center font-medium text-lg">
+                      <span>Risparmio il </span>
+                      <span className=" font-medium text-blue-400 ml-2">
+                        -{item?.discount}%
+                      </span>
+                    </div>
+                    <div className="flex items-center justify-center ">
+                      <span className="text-lg px-2 line-through text-gray-600">
+                        € {item?.price}
+                      </span>
+                      <span className="text-lg font-bold px-2">
+                        € {item?.discountPrice}
+                      </span>
+                    </div>
+                  </div>
+                  <div className="  flex-col absolute hidden group-hover:flex transition-all  top-5 -left-6 ">
+                    <button className="my-2 flex rounded-full items-center justify-center text-base text-gray-600 p-1 bg-yellowCustom-100">
+                      <AiOutlineShoppingCart className="" />
+                      <span className="ml-2 uppercase">Aggiungi</span>
+                    </button>
+
+                    <button className="my-2 p-1 min-w-[15rem]  rounded-full flex items-center text-base justify-center text-blueCustom-100 border-2 border-blueCustom-100">
+                      <AiOutlineHeart className="" />
+                      <span className="ml-2 uppercase">
+                        Aggiungi al whishlist
+                      </span>
+                    </button>
+                  </div>
                 </div>
               </div>
 
