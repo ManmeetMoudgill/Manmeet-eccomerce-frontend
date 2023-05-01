f9a36fcc5608168be57d6056bda420d395197c76
Navi <navbhartimoudgill123@gmail.com>
2023-05-01 10:40:15 +0200

    files added

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,17 +4,32 @@ import OffersCarousel from "@/components/home/offers-carousel";
 import OffersSection from "@/components/home/offers-section";
 import RecomendationOffers from "@/components/home/recomendation-offers";
 import ServicesInformationSection from "@/components/home/services-information";
-
+import { useEffect, useState } from "react";
+import { ImSpinner9 } from "react-icons/im";
 export default function Home() {
+  const [isPageReady, setIsPageReady] = useState<boolean>(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsPageReady(true);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <>
-      <main className="flex flex-col ">
-        <OffersCarousel />
-        <OffersSection />
-        <BestOffers />
-        <ServicesInformationSection />
-        <RecomendationOffers />
-      </main>
+      {!isPageReady ? (
+        <div className="w-full h-screen flex justify-center  pt-[10%]">
+          <ImSpinner9 className="text-blueCustom-100 animate-spin text-5xl" />
+        </div>
+      ) : (
+        <main className="flex flex-col ">
+          <OffersCarousel />
+          <OffersSection />
+          <BestOffers />
+          <ServicesInformationSection />
+          <RecomendationOffers />
+        </main>
+      )}
     </>
   );
 }
