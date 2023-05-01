21024d9224a6fa86d826797d735faf54b3cbc3d6
Navi <navbhartimoudgill123@gmail.com>
2023-05-01 15:32:08 +0200

    files added

--- components/home/company-base-shop.tsx
+++ components/home/company-base-shop.tsx
@@ -13,7 +13,7 @@ const CompanyBaseShop = () => {
     <section className="flex flex-col mb-6">
       <CompanyBaseShopCarousel getCarouselRef={getCarouselRef} />
       <div className="  flex justify-center items-center">
-        <div className=" w-4/5  flex flex-wrap">
+        <div className="w-full px-2 md:px-0 sm:w-4/5  flex flex-col md:flex-row flex-wrap">
           {companiesShop?.map((item, index) => {
             return (
               <div
@@ -24,9 +24,9 @@ const CompanyBaseShop = () => {
                 key={item?.id}
                 className={` ${
                   index === activeIndex ? "bg-gray-100" : ""
-                } flex cursor-pointer justify-center items-center group  w-[19rem] h-[7rem] hover:bg-gray-100 rec-dot`}
+                } flex cursor-pointer justify-center items-center group my-4 sm:my-0  h-[6rem] sm:w-[19rem]  sm:h-[7rem] hover:bg-gray-100 rec-dot`}
               >
-                <span className="text-black text-3xl  uppercase group-hover:text-blueCustom-100  transition-all delay-75">
+                <span className="text-black text-xl md:text-3xl  uppercase group-hover:text-blueCustom-100  transition-all delay-75">
                   {item?.slug}
                 </span>
               </div>
