2892247401fdfc39a3e907f2ddc65daf329eaa47
Navi <navbhartimoudgill123@gmail.com>
2023-05-07 12:06:20 +0200

    files added

--- pages/informatica/index.tsx
+++ pages/informatica/index.tsx
@@ -1,7 +1,27 @@
 import React from "react";
-
 const InformaticaPage = () => {
-  return <div>InformaticaPage</div>;
+  return (
+    <main>
+      <section className="">
+        <div className="flex bg-gradient-to-t shadow-inner   from-gray-100 justify-center items-center">
+          <div className="w-10/12">
+            <h2 className="py-4 px-3 text-gray-500 font-extralight ">
+              Informatica
+            </h2>
+          </div>
+        </div>
+        <div
+          className="w-full h-[50vh]"
+          style={{
+            backgroundImage: `url(/assets/informatica.jpg)`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+          }}
+        />
+      </section>
+    </main>
+  );
 };
 
 export default InformaticaPage;
