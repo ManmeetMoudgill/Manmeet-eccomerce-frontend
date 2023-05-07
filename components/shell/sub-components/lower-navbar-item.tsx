13f9065871869544437ab45968f8b65abc6cb2c2
Navi <navbhartimoudgill123@gmail.com>
2023-05-07 14:57:27 +0200

    files added

--- components/shell/sub-components/lower-navbar-item.tsx
+++ components/shell/sub-components/lower-navbar-item.tsx
@@ -9,9 +9,9 @@ const LowerNavbarItem = ({ title }: LowerNavbarItemProps) => {
       href="/informatica"
       className=" group hidden xl:block    font-medium  cursor-pointer  py-1 mr-16 3xl:mr-20"
     >
-      <a className=" text-blueCustom-100  text-lg 2xl:text-xl tracking-wider  ">
+      <p className=" text-blueCustom-100  text-lg 2xl:text-xl tracking-wider  ">
         {title}
-      </a>
+      </p>
       <div
         className="
                               w-0 group-hover:w-full transition-all ease-in-out duration-700 bg-blueCustom-100 h-1 mt-1"
